93264353f861264679c7fe3e8f7ec852e14cdd2a
Julian Duque <julianduquej@gmail.com>
2015-12-02 02:02:04 -0500

    Add basic support for socket.io
    - Add socket.io and socket.io-client as dependencies
    - Refactor server code to support socket.io
    - Use socket.io-client on browser code to connect to server

--- src/client/tv-shows-container/index.js
+++ src/client/tv-shows-container/index.js
@@ -3,17 +3,21 @@
  */
 
 import $ from 'jquery'
+import socketio from 'socket.io-client'
 
-var $tvShowsContainer = $('#app-body').find('.tv-shows')
+let socket = socketio()
+socket.emit('ping')
+
+let $tvShowsContainer = $('#app-body').find('.tv-shows')
 
 $tvShowsContainer.on('click', 'button.like', function (ev) {
-  var $this = $(this)
-  var $article = $this.closest('.tv-show')
-  var id = $article.data('id')
+  let $this = $(this)
+  let $article = $this.closest('.tv-show')
+  let id = $article.data('id')
   $.post('/api/vote/' + id, function () {
-    var counter = $this.closest('article').find('.count')
-    var content = counter.html()
-    var count = Number(content)
+    let counter = $this.closest('article').find('.count')
+    let content = counter.html()
+    let count = Number(content)
     count = count + 1
     counter.html(count)
     $article.toggleClass('liked')
--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,25 @@
+import http from 'http'
 import express from 'express'
-import api from 'src/server/api'
 import mongoose from 'mongoose'
+import socketio from 'socket.io'
+import api from 'src/server/api'
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 const port = process.env.PORT || 3000
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/tvify')
 
 app.use(express.static('public'))
-
 app.use('/api', api)
 
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+io.on('connection', (socket) => {
+  console.log(`Connected ${socket.id}`)
+
+  socket.on('ping', _ => {
+    console.log('PONG')
+  })
+})
+
+server.listen(port, () => console.log(`Server listening on port ${port}`))
